66f16ff6fdb783dfa2295875e655d17af0aa30c5
carollpq <107604758+carollpq@users.noreply.github.com>
2025-01-30 17:56:33 +0800

    Set Up CSS Configurations
    tailwind.config.js file was added to integrate tailwind CSS into the project. Some properties were added and modified in globals.css (adding colour variable) and in layout.js (changing the main font)

---
+++ src/components/ui/button.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Button = ({text}) => {
+    return (
+        <div className="p-3">
+            <span>{text}</span>
+        </div>
+    )
+}
+
+export default Button;
---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)", 
+        foreground: "var(--foreground)",
+      },
+      backgroundImage: {
+        /* to be filled with the path to the background image */
+      },
+      dropShadow: {
+        'default': '10px 10px 20px rgba(0, 0, 0, 0.25)'
+      },
+      keyframes: {
+        slideUp: {
+          '0%': { opacity: '0', transform: 'translateY(20px)' },
+          '100%': { opacity: '1', transform: 'translateY(0)' },
+        },
+      },
+      animation: {
+        slideUp: 'slideUp 0.7s ease-out',
+      },
+    },
+  },
+  plugins: [],
+};
