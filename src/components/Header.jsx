056332213fbb323e86af1fbd983624efbb4b72e6
carollpq <107604758+carollpq@users.noreply.github.com>
2025-02-02 12:09:05 +0800

    Implement the Functionality of the Navigation Buttons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,32 @@
-
+"use client";
 import Button from "./ui/Button";
 import styles from "../../styles/Header.module.css";
 import ProfileBtn from "./ProfileBtn";
+import { useRouter, usePathname } from "next/navigation";
 
 const Header = () => {
+  // Used for navigating pages
+  const router = useRouter();
+  // Tracks the current page that the user is on
+  const pathname = usePathname();
+
   return (
     <div className={styles["header-container"]}>
       {/* Logo on the top left */}
       <div className={styles["logo"]}>WasteNot</div>
       {/* Container for the "Map" and "Messages" buttons on the top center */}
       <div className={styles["navigation-btn-container"]}>
-        <Button text="Map"/>
-        <Button text="Messages" />
+        {/* Button is highlighted based on user's current page */}
+        <Button
+          text="Map"
+          isActive={pathname == "/"}
+          onClick={() => router.push("/")}
+        />
+        <Button
+          text="Messages"
+          isActive={pathname == "/messages"}
+          onClick={() => router.push("/messages")}
+        />
       </div>
       {/* Profile button on the top right */}
       <ProfileBtn />
