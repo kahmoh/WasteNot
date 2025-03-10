7d439911f9b44b8272208c6ca282ea32296d7b8b
carollpq <107604758+carollpq@users.noreply.github.com>
2025-03-10 22:44:19 +0800

    Modifiy main.jsx to implement React routing, add App component to set up the routing pages

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <Routes>
+        <Route />
+    </Routes>
+  )
+}
+
+export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,14 @@
+import React from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
-    <p>hi</p>
+  // Wrapper that helps identify potential problems ; doesn't render anything on UI
+  <React.StrictMode>
+    {/* Enables client-side routing */}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
