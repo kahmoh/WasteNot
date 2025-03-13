d0151c32e77fd7c08a6562a76eefb14059af7d89
kahmoh <joshmcfadden17@gmail.com>
2025-03-13 19:50:31 +0000

    add height, width and border radius styling rules to map component

--- src/map/Map.jsx
+++ src/map/Map.jsx
@@ -1,20 +1,22 @@
 import {MapContainer, TileLayer, Marker, Popup} from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import './styles/Map.css'
 
 const Map = () => {
-  return (
-      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} style={{width: 1920, height: 1080}}>
-        <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={[51.505, -0.09]}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
-      </MapContainer>
-  )
+    return (
+        <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}
+                      className={'map'}>
+            <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={[51.505, -0.09]}>
+                <Popup>
+                    A pretty CSS3 popup. <br /> Easily customizable.
+                </Popup>
+            </Marker>
+        </MapContainer>
+    )
 }
 
 export default Map
