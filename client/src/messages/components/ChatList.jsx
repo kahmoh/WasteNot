58a3f219d0f9c96bf93c69e80efa66043a086722
carollpq <107604758+carollpq@users.noreply.github.com>
2025-06-16 14:57:52 +0800

    Move all frontend codes into a separate 'client' directory

--- client/src/messages/components/ChatList.jsx
+++ client/src/messages/components/ChatList.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import ChatItem from "./ChatItem.jsx";
 import styles from "../styles/ChatList.module.css";
 
