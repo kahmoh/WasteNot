c4e8892be14783cd720c10eef77ff897e80b4acc
Josh McFadden <joshmcfadden17@gmail.com>
2025-06-17 21:40:30 +0100

    add unstyled filter buttons to navbar

--- src/map/components/NavigationBar.jsx
+++ src/map/components/NavigationBar.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 
+function NavBarQuickFilter(props) {
+    return (
+        <button className="nav-bar-quick-filter">
+            {props.text}
+        </button>
+    )
+}
+
 function NavigationBar() {
   return (
     <div className={"navigation-bar"}>
       <input type={"text"} placeholder={"Search food item here"}/>
+        <NavBarQuickFilter text="All"/>
+        <NavBarQuickFilter text="Food Banks"/>
+        <NavBarQuickFilter text="Vegan"/>
+        <NavBarQuickFilter text="Halal"/>
     </div>
   )
 }
