a365658299103b0ede87bb76bf6c86cce7292934
Josh McFadden <joshmcfadden17@gmail.com>
2025-06-21 14:32:03 +0100

    style navbar -add filter menu button -style navbar quick filter menu button -style nav bar search bar -style navbar filter menu button

--- src/map/components/NavigationBar.jsx
+++ src/map/components/NavigationBar.jsx
@@ -19,10 +19,22 @@ function NavBarFilterRow() {
     )
 }
 
+function NavBarTopRow() {
+    return (
+        <div className="nav-bar-top-row">
+            <div className={"search-container"}>
+                <img src="/search_icon.png" alt=""/>
+                <input type={"text"} placeholder={"Search food item here"} className={"navbar-search"}/>
+            </div>
+            <btn className="filter-menu-button"/>
+        </div>
+    )
+}
+
 function NavigationBar() {
   return (
     <div className={"navigation-bar"}>
-      <input type={"text"} placeholder={"Search food item here"}/>
+        <NavBarTopRow/>
         <NavBarFilterRow/>
     </div>
   )
